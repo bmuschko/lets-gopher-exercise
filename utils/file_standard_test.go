d1fa8d2a073ac449130a5b80b30e47ad8e04b8a5
Benjamin Muschko <benjamin.muschko@gmail.com>
2019-01-27 08:28:19 -0700

    Add tests using the testing package

---
+++ utils/file_standard_test.go
@@ -0,0 +1,57 @@
+package utils_test
+
+import (
+	. "github.com/bmuschko/lets-gopher-exercise/utils"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestCreateDirForNonExistentDirectoryWithTestingPackage(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test")
+	if err != nil {
+		t.Errorf("Failed to create temporary directory %s", dir)
+	}
+	defer os.RemoveAll(dir)
+
+	newDir := filepath.Join(dir, "new")
+	err = CreateDir(newDir)
+	if err != nil {
+		t.Errorf("Failed to create new directory %s", newDir)
+	}
+
+	fileInfo, err := os.Stat(newDir)
+	if err != nil {
+		t.Errorf("Expected directory to %s to be created", newDir)
+	}
+	if fileInfo.Mode().Perm() != os.FileMode(0755) {
+		t.Errorf("Expected directory permissions 0755 but received %s", fileInfo.Mode().Perm())
+	}
+}
+
+func TestCreateDirForExistentDirectoryWithTestingPackage(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test")
+	if err != nil {
+		t.Errorf("Failed to create temporary directory %s", dir)
+	}
+	defer os.RemoveAll(dir)
+
+	newDir := filepath.Join(dir, "new")
+	err = os.Mkdir(newDir, 0755)
+	if err != nil {
+		t.Errorf("Failed to create new directory %s", newDir)
+	}
+	err = CreateDir(newDir)
+	if err != nil {
+		t.Errorf("Failed to create new directory %s", newDir)
+	}
+
+	fileInfo, err := os.Stat(newDir)
+	if err != nil {
+		t.Errorf("Expected directory to %s to be created", newDir)
+	}
+	if fileInfo.Mode().Perm() != os.FileMode(0755) {
+		t.Errorf("Expected directory permissions 0755 but received %s", fileInfo.Mode().Perm())
+	}
+}
